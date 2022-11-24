782717314ec2b900fdbc7fe213c13bdb3c168951
Callum Andrew <calcium@mailbox.org>
2022-11-24 19:53:24 +1100

    Change structure and add bash completion

---
+++ completions/hooroo.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Hooroo bash completion
+
+_hooroo_completions() {
+	# Complete commands in first argument, and options after
+	if [[ ${#COMP_WORDS[@]} -lt 3 ]]; then
+		completions="apply save"
+	else
+		completions="-h --help -l --location -m --mode --noconfirm --yes"
+	fi
+
+	# Save completions list
+	COMPREPLY=($(compgen -W "$completions" -- ${COMP_WORDS[$COMP_CWORD]}))
+}
+
+complete -F _hooroo_completions hooroo
