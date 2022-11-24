eec5c741e07b70564ae225570e9dc234500b007b
Callum Andrew <calcium@mailbox.org>
2022-11-24 22:37:16 +1100

    Improve bash completion and condense code

--- completions/hooroo.bash
+++ completions/hooroo.bash
@@ -1,16 +1,32 @@
-#!/bin/bash
 # Hooroo bash completion
 
 _hooroo_completions() {
+	# Unset previous variables
+	unset completions file
+
 	# Complete commands in first argument, and options after
-	if [[ ${#COMP_WORDS[@]} -lt 3 ]]; then
+	if (( $COMP_CWORD == 1 )); then
 		completions="apply save"
 	else
-		completions="-h --help -l --location -m --mode --noconfirm --yes"
+		# Find previous option used
+		prev_opt=$(( $COMP_CWORD - 1 ))
+
+		# Use different completions for previous options used
+		case ${COMP_WORDS[$prev_opt]} in
+			-l | --location)
+				file=f
+				;;
+			-m | --mode)
+				completions="all apt flatpak pacman"
+				;;
+			*)
+				completions="-h --help -l --location -m --mode --noconfirm --yes"
+				;;
+		esac
 	fi
 
 	# Save completions list
-	mapfile -t COMPREPLY < <(compgen -W "$completions" -- "${COMP_WORDS[$COMP_CWORD]}")
+	mapfile -t COMPREPLY < <(compgen -"$file"W "$completions" -- "${COMP_WORDS[$COMP_CWORD]}")
 }
 
 complete -F _hooroo_completions hooroo
